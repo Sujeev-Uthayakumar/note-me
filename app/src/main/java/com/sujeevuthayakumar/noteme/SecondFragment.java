984cafcd553a04a0dd88010f527199c2316eb1cb
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2023-10-06 00:13:02 -0400

    add notes ui

--- app/src/main/java/com/sujeevuthayakumar/noteme/SecondFragment.java
+++ app/src/main/java/com/sujeevuthayakumar/noteme/SecondFragment.java
@@ -1,9 +1,15 @@
 package com.sujeevuthayakumar.noteme;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,8 +26,10 @@ public class SecondFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding.editNote.setBackgroundColor(getResources().getColor(R.color.lightblue));
+        binding.blue.setBackgroundColor(getResources().getColor(R.color.purple));
+        binding.blue.setTextColor(Color.WHITE);
         return binding.getRoot();
 
     }
@@ -32,10 +40,93 @@ public class SecondFragment extends Fragment {
         binding.submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                if (!areInputsEmpty(view)) {
+                    System.out.println("it works");
+                }
+                styleErrorInputs(view);
             }
         });
+
+        binding.radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                resetButtons(view);
+                RadioButton checkedButton = view.findViewById(checkedId);
+                EditText editText = view.findViewById(R.id.edit_note);
+                if (checkedId == R.id.blue) {
+                    checkedButton.setBackgroundColor(getResources().getColor(R.color.purple));
+                    checkedButton.setTextColor(Color.WHITE);
+                    editText.setBackgroundColor(getResources().getColor(R.color.lightblue));
+                } else if (checkedId == R.id.red) {
+                    checkedButton.setBackgroundColor(getResources().getColor(R.color.purple));
+                    checkedButton.setTextColor(Color.WHITE);
+                    editText.setBackgroundColor(getResources().getColor(R.color.lightred));
+                } else if (checkedId == R.id.yellow) {
+                    checkedButton.setBackgroundColor(getResources().getColor(R.color.purple));
+                    checkedButton.setTextColor(Color.WHITE);
+                    editText.setBackgroundColor(getResources().getColor(R.color.lightyellow));
+                } else {
+                    checkedButton.setBackgroundColor(getResources().getColor(R.color.purple));
+                    checkedButton.setTextColor(Color.WHITE);
+                    editText.setBackgroundColor(getResources().getColor(R.color.lightblue));
+                }
+            }
+        });
+    }
+
+    private void resetButtons(View view) {
+        RadioButton blue = view.findViewById(R.id.blue);
+        RadioButton red = view.findViewById(R.id.red);
+        RadioButton yellow = view.findViewById(R.id.yellow);
+
+        blue.setTextColor(Color.BLACK);
+        red.setTextColor(Color.BLACK);
+        yellow.setTextColor(Color.BLACK);
+
+        blue.setBackgroundColor(getResources().getColor(R.color.lightblue));
+        red.setBackgroundColor(getResources().getColor(R.color.lightred));
+        yellow.setBackgroundColor(getResources().getColor(R.color.lightyellow));
+    }
+
+    private boolean areInputsEmpty(View view) {
+        String titleText = binding.editTitle.getText().toString();
+        return titleText.isEmpty();
+    }
+
+    // Function to style inputs based on whether they are required
+    private void styleErrorInputs(View view) {
+        // Get the values of inputs
+        String titleText = binding.editTitle.getText().toString();
+
+        // Color for input error
+        int color = Color.rgb(255,0,0);
+        int color2 = Color.parseColor("#FF11AA");
+
+        // The states for an input when clicked, and hovered
+        int[][] states = new int[][] {
+                new int[] { android.R.attr.state_focused},
+                new int[] { android.R.attr.state_hovered},
+                new int[] { android.R.attr.state_enabled},
+                new int[] { }
+        };
+
+        // Set all possible colors
+        int[] colors = new int[] {
+                color,
+                color,
+                color,
+                color2
+        };
+
+        // The list of colors used for the state
+        ColorStateList myColorList = new ColorStateList(states, colors);
+
+        // Check for principal amount input
+        if (titleText.isEmpty()) {
+            binding.editTitleLayout.setError("Required");
+        } else {
+            binding.editTitleLayout.setError(null);
+        }
     }
 
     @Override
